968e9736aa91ab729fdfa5f21122b898574ef3ed
Yehezkiel Hamzah <hamzah.yehezkiel@gmail.com>
2022-03-16 17:31:09 -0700

    db seeded

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const Sequelize = require ('sequelize');
+const sequelize = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/shop')
 
 app.use('/dist', express.static(path.join(__dirname, 'dist')));
 app.get('/', (req, res)=> res.sendFile(path.join(__dirname, 'index.html')));
 
-const port = process.env.PORT || 3000;
 
-app.listen(port, ()=> console.log(`listening on port ${port}`));
+
+
+
+const init = async () => {
+  try {
+    await sequelize.sync({force:true})
+
+    const port = process.env.PORT || 3000;
+    app.listen(port, ()=> console.log(`listening on port ${port}`));
+  }
+  catch (e) {
+    console.log(e)
+  }
+}
+
+init()
